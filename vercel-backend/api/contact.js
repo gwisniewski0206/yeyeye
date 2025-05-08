fe71703e8c4018ad6eb0a12ad322d41ce60a9f6f
gwisniewski0206 <g.wisniewski@t2consult.net>
2025-05-08 13:22:09 +0200

    Update contact.js

--- vercel-backend/api/contact.js
+++ vercel-backend/api/contact.js
@@ -45,6 +45,6 @@ Message: ${message}`,
     return res.status(200).json({ message: 'Email sent successfully' });
   } catch (err) {
     console.error(err);
-    return res.status(200).json({ error: 'Internal server error' });
+    return res.status(500).json({ error: 'Internal server error' });
   }
 }
